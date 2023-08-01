a23238917def86aed4847943614cd23e5511c790
Ulises Cappa <ulisescappa@gmail.com>
2023-08-01 18:37:52 -0300

    Fixing linters erros

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './style.css';
-import Leaderboard from './scores';
+import { Leaderboard } from './scores.js';
 
-let leaderboard = new Leaderboard()
+const leaderboard = new Leaderboard();
 
-leaderboard.addScore()
+leaderboard.addScore();
 
 window.onload = () => {
-    leaderboard.loadData();
-}
+  leaderboard.loadData();
+};
--- src/scores.js
+++ src/scores.js
@@ -1,41 +1,40 @@
-const scoreList = document.querySelector('.score-list')
-const scoreForm = document.querySelector('.score-form')
+const scoreList = document.querySelector('.score-list');
+const scoreForm = document.querySelector('.score-form');
 
 export class Leaderboard {
     data = []
 
     addData(s) {
-        this.data.push(s)
+      this.data.push(s);
     }
 
     createScore(nameParam, scoreParam, ind = 0) {
-        const scoreObj = {
-            index: ind,
-            name: nameParam,
-            score: scoreParam
-        };
-        this.addData(scoreObj);
+      const scoreObj = {
+        index: ind,
+        name: nameParam,
+        score: scoreParam,
+      };
+      this.addData(scoreObj);
     }
 
     displayData() {
-        scoreList.innerHTML = ``
-        this.data.forEach(elem => {
-            scoreList.innerHTML += `
+      scoreList.innerHTML = '';
+      this.data.forEach((elem) => {
+        scoreList.innerHTML += `
             <li class="item">${elem.name}: ${elem.score}</li>
-            `
-        })
+            `;
+      });
     }
 
     addScore = () => {
-        scoreForm.addEventListener('submit', (event) => {
-            event.preventDefault();
-            const name = document.querySelector('#input-name');
-            const score = document.querySelector('#input-score');
-            this.createScore(name.value,score.value)
-            this.displayData()
-        })
+      scoreForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const name = document.querySelector('#input-name');
+        const score = document.querySelector('#input-score');
+        this.createScore(name.value, score.value);
+        this.displayData();
+      });
     }
-
 }
 
 export default Leaderboard;
